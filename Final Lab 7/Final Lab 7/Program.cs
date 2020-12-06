ce9720f9ea948137dfb87504092c7dc6b4c2bf71
Shushmita Chowdhury <anamica.chowdhury31@gmail.com>
2020-12-06 15:36:46 +0600

    Final Lab 7 added

---
+++ Final Lab 7/Final Lab 7/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Final_Lab_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BasicCalculatorImplement x = new BasicCalculatorImplement();
+            Console.WriteLine(x.sub(3, 2));
+            Console.WriteLine(x.sum(3, 2));
+
+            ScientificCalculatorImplement y = new ScientificCalculatorImplement();
+            Console.WriteLine(y.Exponential(2, 2));
+            Console.WriteLine(y.Square(5));
+            Console.WriteLine(y.Sine(5, 45));
+
+
+        }
+    }
+}
---
+++ Final Lab 7/Final Lab 7/ScientificCalculatorInterface.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Final_Lab_7
+{
+    interface ScientificCalculatorInterface
+    {
+        Double Exponential(double x, double y);
+        int Square(int x);
+        int Qube(int x);
+        double Sine(double x, double angle);
+        double Cos(double x, double angle);
+    }
+}
