81943fec56414fc86cd3d281cf608b8bacb53e41
Shushmita Chowdhury <anamica.chowdhury31@gmail.com>
2020-12-06 13:39:43 +0600

    Final Lab 2 added

---
+++ Final Lab 2/Final Lab 2/Faculty.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Final_Lab_2
+{
+    class Faculty
+    {
+        public string Name { get; set; }
+        public string Id { get; set; }
+        public Faculty(string name, string id)
+        {
+            Name = name;
+            Id = id;
+        }
+        public void ShowInfo()
+        {
+            Console.WriteLine("Teacher's Name  :" + Name);
+            Console.WriteLine("Teacher's ID  :" + Id);
+
+        }
+    }
+}
